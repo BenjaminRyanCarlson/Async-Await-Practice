f8661ee33ca6db416aee03a89be10fdf33c780fa
Benjamin Carlson <benjamincarlson3.0@gmail.com>
2020-12-10 17:02:34 -0600

    initial

---
+++ script.js
@@ -0,0 +1,43 @@
+
+
+printRandomNumber()
+.then(() => printAllCities());
+
+async function printRandomNumber(){
+    const randNumber = await getRandomNumber();
+    const randNumberLog = `Random Number: ${randNumber}`;
+    console.log(randNumberLog);
+   
+}
+
+async function printAllCities(){
+    for (const city of [ 'dallas', 'austin', 'new_orleans', 'shreveport']) {
+        await printLatitudeAndLongitude(city);
+    }
+}
+
+
+function getRandomNumber(){
+    return new Promise(x =>
+        setTimeout(() => x(Math.floor(Math.random() * 1000)), 500));
+}
+
+async function printLatitudeAndLongitude(city) {
+    const url = `https://geocode.xyz/${city}?json=1`;
+  
+  let res = await fetch(url);
+  
+  if (res.status != 200) {
+    res = await new Promise(x => setTimeout(async () => {
+        const tmpRes = await fetch(url);
+        x(tmpRes);
+      }, 1000));
+  }
+
+    const json = await res.json();
+  const output = `${json.standard.city} - Latitude: ${json.latt}, Longitude: ${json.longt}`;
+  console.log(output);
+  
+}
+
+
